60c9a3ad2ae55e6e07847e5f575abf2ff223f744
kratos619 <gaurav.palaspagar@hotmail.com>
2019-08-04 12:13:37 +0530

    ad basic code

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,52 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['foos tester'];
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("ohhh yesss"),
-          ),
-          body: Column(
-            children: [
-              Container(
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add("advance Food Tester");
-                      print(_products);
-                    });
-                  },
-                  child: Text("add Product"),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map(
-                      (element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset("assets/food.jpg"),
-                            Text(element),
-                          ],
-                        ),
-                      ),
-                    )
-                    .toList(),
-              )
-            ],
-          )),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: Container(),
+    ),
+  ));
 }
